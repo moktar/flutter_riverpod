9c61d009f2e997f0d7c17c5704be234154a31d66
Moktar Hossain <moktarhossain@outlook.com>
2026-01-19 16:38:05 +0600

    Update riverpod imports to hooks_riverpod in todos example

---
+++ examples/random_number/lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: RandomNumberApp()));
+}
+
+// Notifier for generating a random number exposed by a state notifier
+// provider
+class RandomNumberGenerator extends Notifier<int> {
+  @override
+  int build() => Random().nextInt(9999);
+
+  void generate() {
+    state = Random().nextInt(9999);
+  }
+}
+
+// Notifier provider holding the state
+final randomNumberProvider = NotifierProvider<RandomNumberGenerator, int>(
+  RandomNumberGenerator.new,
+);
+
+class RandomNumberApp extends StatelessWidget {
+  const RandomNumberApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Random number')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const RandomConsumer(),
+              // Consumer to call a method inside StateNotifier just to change
+              // the state
+              Consumer(
+                builder: (context, ref, child) {
+                  return ElevatedButton(
+                    child: const Text('Generate'),
+                    onPressed:
+                        () =>
+                        ref.read(randomNumberProvider.notifier).generate(),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Custom consumer using the provider
+class RandomConsumer extends ConsumerWidget {
+  const RandomConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Text(ref.watch(randomNumberProvider).toString());
+  }
+}
